760a6fcdf3871d7dad4046b4bf1ce87f7a12db96
John Carlo Joyo <john_carlo_joyo@dlsu.edu.ph>
2022-10-25 01:51:06 +0800

    added skeleton codes for about us page

---
+++ components/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+import Logo from "../../public/assets/about-us_logo.png";
+
+const AboutPage = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default AboutPage;
---
+++ pages/about-us/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutPage from "../../components/AboutPage/AboutPage";
+
+function App() {
+    return(
+        <AboutPage />
+    );
+};
+
+export default App;
