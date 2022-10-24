7e28c7fe96678646027d17f3c2bfeb27d449c8c1
zen-young <renzcruz50@gmail.com>
2022-10-24 17:22:28 +0800

    implemented simple header for product list page

---
+++ components/ProductList/ProductListHeader.js
@@ -0,0 +1,11 @@
+export default function ProductListHeader() {
+  return (
+    <>
+      <header className="mt-9">
+        <div className="pb-7 text-center">
+          <h1 className="font-bold">Product List</h1>
+        </div>
+      </header>
+    </>
+  );
+}
