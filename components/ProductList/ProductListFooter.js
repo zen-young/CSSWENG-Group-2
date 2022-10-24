31dd1b608132ba9e9c3483cb8bb848185d105763
zen-young <renzcruz50@gmail.com>
2022-10-24 15:47:29 +0800

    implemented product list page footer

---
+++ components/ProductList/ProductListFooter.js
@@ -0,0 +1,21 @@
+export default function ProductListFooter() {
+  return (
+    <>
+      <footer className="mb-9 mt-20">
+        <div className="flex justify-center">
+          <hr style={{ border: "1px solid rgba(0, 0, 0, 0.3)" }} width="92%" />
+        </div>
+        <div className="pt-7">
+          <div className="text-center">
+            <h3>
+              Can&apos;t find what you&apos;re looking for? Contact us on Viber
+            </h3>
+            <h3>
+              at <span className="font-bold">+63917-510-1023</span>
+            </h3>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
