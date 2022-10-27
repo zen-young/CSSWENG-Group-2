d50dffbf20cc8b8b432b378cfdfe31ff8d2be5f3
Matthew Villarica <matthewvillarica@gmail.com>
2022-10-27 13:32:52 +0800

    unit test product page

--- __tests__/productpage.test.jsx
+++ __tests__/productpage.test.jsx
@@ -14,22 +14,19 @@ window.ResizeObserver =
 global.CSSObject = jest.fn();
 
 describe("Product Page", () => {
-  //test
+  let labelSectionData;
+
+  beforeEach(() => {
+    labelSectionData = {
+      productName: "Notepads",
+      quantities: ["5", "10", "20", "40", "80", "150"],
+      sizes: ["2.4x1.25", "2.4x2", "3.5x2"],
+      prices: [40, 60, 80],
+    };
+  });
+
   it("Displayed rice changes according to quantity, size and price combination", () => {
-    const quantities = ["5", "10", "20", "40", "80", "150"];
-
-    const sizes = ["2.4x1.25", "2.4x2", "3.5x2"];
-
-    const prices = [40, 60, 80];
-
-    render(
-      <LabelSection
-        productName={"Notepads"}
-        sizes={sizes}
-        quantities={quantities}
-        prices={prices}
-      />
-    );
+    render(<LabelSection {...labelSectionData} />);
 
     const name = screen.getByText("Notepads");
     const dropDown = screen.getAllByAltText("down");
@@ -40,16 +37,19 @@ describe("Product Page", () => {
 
     fireEvent.click(sizeSelect);
 
-    fireEvent.click(screen.getByText(sizes[2]));
+    fireEvent.click(screen.getByText(labelSectionData.sizes[2]));
 
     fireEvent.click(qtySelect);
 
-    fireEvent.click(screen.getByText(quantities[2] + " Pieces"));
+    fireEvent.click(
+      screen.getByText(labelSectionData.quantities[2] + " Pieces")
+    );
 
     expect(name).toBeVisible();
 
     const correctPrice = screen.getByText(
-      "₱" + (prices[2] * quantities[2]).toFixed(2)
+      "₱" +
+        (labelSectionData.prices[2] * labelSectionData.quantities[2]).toFixed(2)
     );
 
     expect(correctPrice).toBeVisible();
@@ -58,14 +58,17 @@ describe("Product Page", () => {
 
     fireEvent.click(sizeSelect);
 
-    fireEvent.click(screen.getByText(sizes[1]));
+    fireEvent.click(screen.getByText(labelSectionData.sizes[1]));
 
     fireEvent.click(qtySelect);
 
-    fireEvent.click(screen.getByText(quantities[4] + " Pieces"));
+    fireEvent.click(
+      screen.getByText(labelSectionData.quantities[4] + " Pieces")
+    );
 
     const correctPrice2 = screen.getByText(
-      "₱" + (prices[1] * quantities[4]).toFixed(2)
+      "₱" +
+        (labelSectionData.prices[1] * labelSectionData.quantities[4]).toFixed(2)
     );
 
     expect(correctPrice2).toBeVisible();
@@ -74,14 +77,17 @@ describe("Product Page", () => {
 
     fireEvent.click(sizeSelect);
 
-    fireEvent.click(screen.getByText(sizes[0]));
+    fireEvent.click(screen.getByText(labelSectionData.sizes[0]));
 
     fireEvent.click(qtySelect);
 
-    fireEvent.click(screen.getByText(quantities[3] + " Pieces"));
+    fireEvent.click(
+      screen.getByText(labelSectionData.quantities[3] + " Pieces")
+    );
 
     const correctPrice3 = screen.getByText(
-      "₱" + (prices[0] * quantities[3]).toFixed(2)
+      "₱" +
+        (labelSectionData.prices[0] * labelSectionData.quantities[3]).toFixed(2)
     );
 
     expect(correctPrice3).toBeVisible();
