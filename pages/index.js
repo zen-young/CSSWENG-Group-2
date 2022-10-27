56b9824925e46c3e13895210353b556a14da1b4c
zen-young <renzcruz50@gmail.com>
2022-10-27 22:17:20 +0800

    fixed pages directory

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div className="text-center">hello world</div>;
+import React, { useState } from 'react';
+import Carousel_comp from '../components/homepage/Carousel_comp';
+import Featured_Products from '../components/homepage/Featured_Products';
+import About_Us from '../components/homepage/about_us';
+
+
+function Homepage() {
+    const [numberofPages, setNumPages] = useState(3);
+
+    return (
+        <>
+            <Carousel_comp />
+            <Featured_Products numPages={numberofPages}/>
+            <About_Us />
+        </>
+    );
 }
+
+export default Homepage;
