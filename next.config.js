30ce6e354ea33d32e09e21e617018c0f11cc7d5c
zen-young <renzcruz50@gmail.com>
2022-11-27 20:50:24 +0800

    implemented data fetching for products/[id] routes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -2,36 +2,49 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import ImageSection from "../../components/ImageSection/ImageSection";
 import LabelSection from "../../components/LabelSection/LabelSection";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebaseConfig";
 
-export default function Product() {
-  const [content, setContent] = useState("Personalized Stickers with Diecut");
-  const [sizes, setSizes] = useState(["2.4x1.25", "2.4x2", "3.5x2"]);
-  const [prices, setPrices] = useState([40, 60, 80]);
-  const [paperContent, setPaperContent] = useState(
-    "Glossy or matte waterproof and scratch proof vinyl stickers"
-  );
-  const [quantites, setQuantities] = useState([
-    "10",
-    "20",
-    "50",
-    "100",
-    "500",
-    "1000",
-  ]);
-  const router = useRouter();
-  const { id } = router.query;
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const productRef = doc(db, "products", id);
+  const productSnap = await getDoc(productRef);
 
+  if (!productSnap.exists()) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { product: productSnap.data() },
+  };
+}
+
+export default function Product({ product }) {
   return (
     <div className="flex flex-col-reverse sm:flex-row w-full">
       <div className="basis-1/2 p-8">
         <ImageSection
-          content={content}
-          sizes={sizes}
-          paperContent={paperContent}
+          images={product.image_urls.map((url) => {
+            return { src: url };
+          })}
+          content={product.description}
+          sizes={product.product_sizes}
+          paperContent={product.paper_types.join(", ")}
         />
       </div>
       <div className="basis-1/2 p-8">
-        <LabelSection productName={id} sizes={sizes} quantities={quantites} prices={prices}/>
+        <LabelSection
+          productName={product.name}
+          sizes={product.product_sizes}
+          quantities={product.variations.map((variation) => {
+            return variation.quantity;
+          })}
+          prices={product.variations.map((variation) => {
+            return variation.price;
+          })}
+        />
       </div>
     </div>
   );
